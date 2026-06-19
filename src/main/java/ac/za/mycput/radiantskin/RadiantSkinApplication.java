084efa620272195021ed3e3c624c7a95edd5345b
HumphreyMahlangu <godasamukelo757@gmai.com>
2026-06-19 15:35:39 +0200

    Initial commit

---
+++ src/main/java/ac/za/mycput/radiantskin/RadiantSkinApplication.java
@@ -0,0 +1,13 @@
+package ac.za.mycput.radiantskin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiantSkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadiantSkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/za/mycput/radiantskin/RadiantSkinApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.za.mycput.radiantskin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiantSkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
