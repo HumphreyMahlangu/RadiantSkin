a2a178339744ca9d7ca2b6ff3c49f2a78edb2572
nonhlanhlahazel22 <222687290@mycput.ac.za>
2026-06-21 14:26:04 +0200

    Updated Admin and User entities

--- src/main/java/ac/za/mycput/domain/Admin.java
+++ src/main/java/ac/za/mycput/domain/Admin.java
@@ -1,4 +1,28 @@
 package ac.za.mycput.domain;
 
-public class Admin {
+import jakarta.persistence.Entity;
+
+@Entity
+public class Admin extends User {
+
+    private String employeeNumber;
+
+    protected Admin() {
+
+    }
+
+    public Admin(Long userId, String firstName, String lastName, String email,
+                 String password, String employeeNumber) {
+        super(userId, firstName, lastName, email, password);
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getEmployeeNumber() { return employeeNumber; }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "employeeNumber='" + employeeNumber + '\'' +
+                "} " + super.toString();
+    }
 }
--- src/main/java/ac/za/mycput/domain/User.java
+++ src/main/java/ac/za/mycput/domain/User.java
@@ -1,6 +1,43 @@
 package ac.za.mycput.domain;
 
-public class User {
+import jakarta.persistence.*;
 
-    
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User {
+
+    @Id
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    protected User() {
+
+    }
+
+    public User(Long userId, String firstName, String lastName, String email, String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getUserId() { return userId; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
