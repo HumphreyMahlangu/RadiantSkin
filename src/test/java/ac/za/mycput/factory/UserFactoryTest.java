2f5a725222d1daf5ef9e7d20c43d796b1fb05f2e
Nonhlanhla <222687290@mycput.ac.za>
2026-07-15 01:57:17 +0200

    Updated Cart and CartItem

--- src/test/java/ac/za/mycput/factory/UserFactoryTest.java
+++ src/test/java/ac/za/mycput/factory/UserFactoryTest.java
@@ -1,4 +1,63 @@
+package ac.za.mycput.factory;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserFactoryTest {
-  
+
+    @Test
+    void validUser() {
+
+        assertTrue(UserFactory.isValidUser(
+                "Lerato",
+                "Mabena",
+                "lerato.mabena@gmail.com",
+                "Password123"
+        ));
+    }
+
+    @Test
+    void invalidFirstName() {
+
+        assertFalse(UserFactory.isValidUser(
+                "",
+                "Zulu",
+                "siyabonga.zulu@gmail.com",
+                "Password123"
+        ));
+    }
+
+    @Test
+    void invalidLastName() {
+
+        assertFalse(UserFactory.isValidUser(
+                "Nompumelelo",
+                "",
+                "nompumelelo.nkosi@gmail.com",
+                "Password123"
+        ));
+    }
+
+    @Test
+    void invalidEmail() {
+
+        assertFalse(UserFactory.isValidUser(
+                "Sipho",
+                "Mthembu",
+                "siphomthembugmail.com",
+                "Password123"
+        ));
+    }
+
+    @Test
+    void invalidPassword() {
+
+        assertFalse(UserFactory.isValidUser(
+                "Zanele",
+                "Nkosi",
+                "zanele.nkosi@gmail.com",
+                ""
+        ));
+    }
 }
